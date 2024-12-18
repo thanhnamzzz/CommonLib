909df3e747edfa100688bac4377a79e0515a6471
thanhnamzzz <ksthanhnam1995@gmail.com>
2024-12-18 10:59:57 +0700

    update Glide

--- common_libs/src/main/java/io/virgo_common/common_libs/views/ImageGlide.kt
+++ common_libs/src/main/java/io/virgo_common/common_libs/views/ImageGlide.kt
@@ -2,6 +2,7 @@ package io.virgo_common.common_libs.views
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import kotlinx.coroutines.CoroutineExceptionHandler
@@ -24,6 +25,75 @@ fun Context.loadImage(
     }
     CoroutineScope(Dispatchers.Main).launch(exception) {
         Glide.with(this@loadImage).load(image).into(view)
+        complete(true, null)
+        cancel()
+    }
+}
+
+enum class ImageTrans {
+    FIT_CENTER,
+    CENTER_CROP,
+    CENTER_INSIDE,
+    CIRCLE_CROP
+}
+
+fun Context.loadImage(
+    view: ImageView,
+    image: Any,
+    trans: ImageTrans,
+    complete: (complete: Boolean, error: String?) -> Unit
+) {
+    val exception = CoroutineExceptionHandler { _, throwable ->
+        throwable.message?.let {
+            (this as Activity).runOnUiThread {
+                complete(false, it)
+            }
+        }
+    }
+    CoroutineScope(Dispatchers.Main).launch(exception) {
+        when (trans) {
+            ImageTrans.FIT_CENTER -> Glide.with(this@loadImage).load(image).fitCenter().into(view)
+            ImageTrans.CENTER_CROP -> Glide.with(this@loadImage).load(image).centerCrop().into(view)
+            ImageTrans.CENTER_INSIDE -> Glide.with(this@loadImage).load(image).centerInside()
+                .into(view)
+
+            ImageTrans.CIRCLE_CROP -> Glide.with(this@loadImage).load(image).circleCrop().into(view)
+        }
+        complete(true, null)
+        cancel()
+    }
+}
+
+fun Context.loadImage(
+    view: ImageView,
+    image: Any,
+    trans: ImageTrans,
+    placeHolder: Drawable?,
+    error: Any,
+    complete: (complete: Boolean, error: String?) -> Unit
+) {
+    val exception = CoroutineExceptionHandler { _, throwable ->
+        throwable.message?.let {
+            (this as Activity).runOnUiThread {
+                complete(false, it)
+            }
+        }
+    }
+    CoroutineScope(Dispatchers.Main).launch(exception) {
+        when (trans) {
+            ImageTrans.FIT_CENTER -> Glide.with(this@loadImage).load(image).fitCenter()
+                .placeholder(placeHolder).error(error).into(view)
+
+            ImageTrans.CENTER_CROP -> Glide.with(this@loadImage).load(image).centerCrop()
+                .placeholder(placeHolder).error(error).into(view)
+
+            ImageTrans.CENTER_INSIDE -> Glide.with(this@loadImage).load(image).centerInside()
+                .placeholder(placeHolder).error(error)
+                .into(view)
+
+            ImageTrans.CIRCLE_CROP -> Glide.with(this@loadImage).load(image).circleCrop()
+                .placeholder(placeHolder).error(error).into(view)
+        }
         complete(true, null)
         cancel()
     }

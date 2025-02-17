8375c8c07c10d9735d449c7ae25318f48505daa6
Nguyễn Thành Nam <106824405+thanhnamzzz@users.noreply.github.com>
2025-02-17 10:10:47 +0700

    fix SimpleFragment

--- common_libs/src/main/java/io/virgo_common/common_libs/baseApp/SimpleFragment.kt
+++ common_libs/src/main/java/io/virgo_common/common_libs/baseApp/SimpleFragment.kt
@@ -20,8 +20,8 @@ abstract class SimpleFragment<VB : ViewBinding>(private val bindingInflater: (La
         return fBinding.root
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 }

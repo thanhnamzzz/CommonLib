3c7704ded554542e52a6b519c22b623f356e80ee
Nguyễn Thành Nam <106824405+thanhnamzzz@users.noreply.github.com>
2025-04-29 11:15:58 +0700

    update build.gradle.kts libs

--- common_libs/build.gradle.kts
+++ common_libs/build.gradle.kts
@@ -1,51 +1,55 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    id("maven-publish")
+	alias(libs.plugins.android.library)
+	alias(libs.plugins.kotlin.android)
+	id("maven-publish")
 }
 
 android {
-    namespace = "io.virgo_common.common_libs"
-    compileSdk = 35
+	namespace = "io.virgo_common.common_libs"
+	compileSdk = 35
 
-    defaultConfig {
-        minSdk = 21
+	defaultConfig {
+		minSdk = 21
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		consumerProguardFiles("consumer-rules.pro")
+	}
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+	buildTypes {
+		release {
+			isMinifyEnabled = false
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
+		}
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_17
+		targetCompatibility = JavaVersion.VERSION_17
+	}
+	kotlinOptions {
+		jvmTarget = "17"
+	}
 
-    buildFeatures { viewBinding = true }
+	buildFeatures { viewBinding = true }
+
+	lint {
+		baseline = file("lint-baseline.xml")
+	}
 }
 
 publishing.publications {
-    create<MavenPublication>("release") {
-        afterEvaluate {
-            from(components["release"])
-        }
-    }
+	create<MavenPublication>("release") {
+		afterEvaluate {
+			from(components["release"])
+		}
+	}
 }
 
 dependencies {
-    implementation(libs.androidx.appcompat)
-    implementation(libs.sdp.android)
-    //Glide
-    implementation(libs.glide)
+	implementation(libs.androidx.appcompat)
+	implementation(libs.sdp.android)
+	//Glide
+	implementation(libs.glide)
 }

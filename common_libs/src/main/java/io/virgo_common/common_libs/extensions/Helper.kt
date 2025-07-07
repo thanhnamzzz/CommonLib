7d20d55a4fd7af255835a87e53262b0ae832ae93
Nguyễn Thành Nam <106824405+thanhnamzzz@users.noreply.github.com>
2025-07-07 17:13:35 +0700

    update function handler

--- common_libs/src/main/java/io/virgo_common/common_libs/extensions/Helper.kt
+++ common_libs/src/main/java/io/virgo_common/common_libs/extensions/Helper.kt
@@ -3,21 +3,27 @@ package io.virgo_common.common_libs.extensions
 import android.app.Activity
 import android.content.Context
 import android.media.MediaScannerConnection
+import android.os.Handler
+import android.os.Looper
 import android.view.Window
 import android.view.WindowManager
 import io.virgo_common.common_libs.views.MyToast
 
 fun reloadGallerySystem(context: Context, filePath: String) {
-    MediaScannerConnection.scanFile(
-        context, arrayOf(filePath), null
-    ) { _, _ -> }
+	MediaScannerConnection.scanFile(
+		context, arrayOf(filePath), null
+	) { _, _ -> }
 }
 
 fun toastMess(activity: Activity, mess: String, duration: Int, typeToast: MyToast.TypeToast) {
-    when (typeToast) {
-        MyToast.TypeToast.TOAST_SUCCESS -> MyToast.showToastSuccess(activity, mess, duration)
-        MyToast.TypeToast.TOAST_ERROR -> MyToast.showToastError(activity, mess, duration)
-        MyToast.TypeToast.TOAST_WARNING -> MyToast.showToastWarning(activity, mess, duration)
-        MyToast.TypeToast.TOAST_NONE -> MyToast.showToastNone(activity, mess, duration)
-    }
+	when (typeToast) {
+		MyToast.TypeToast.TOAST_SUCCESS -> MyToast.showToastSuccess(activity, mess, duration)
+		MyToast.TypeToast.TOAST_ERROR -> MyToast.showToastError(activity, mess, duration)
+		MyToast.TypeToast.TOAST_WARNING -> MyToast.showToastWarning(activity, mess, duration)
+		MyToast.TypeToast.TOAST_NONE -> MyToast.showToastNone(activity, mess, duration)
+	}
+}
+
+fun handlerFunction(timeWait: Long, callback: () -> Unit) {
+	Handler(Looper.getMainLooper()).postDelayed({ callback() }, timeWait)
 }

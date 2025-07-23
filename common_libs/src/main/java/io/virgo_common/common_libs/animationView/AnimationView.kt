a398fc901a4c0a502831e7e04851eeb3aad78b80
Nguyễn Thành Nam <106824405+thanhnamzzz@users.noreply.github.com>
2025-07-23 21:25:01 +0700

    fix AnimationView

--- common_libs/src/main/java/io/virgo_common/common_libs/animationView/AnimationView.kt
+++ common_libs/src/main/java/io/virgo_common/common_libs/animationView/AnimationView.kt
@@ -8,56 +8,58 @@ import android.animation.AnimatorSet
 import android.view.animation.AccelerateInterpolator
 
 class AnimationView() {
-	var du: Long = 1000
-	private var isLoop: Boolean = false
-	private var delayLoop: Long = 1000
-	private var isPause: Boolean = false
+    var du: Long = 1000
+    private var isLoop: Boolean = false
+    private var delayLoop: Long = 1000
+    private var isPause: Boolean = false
 
-	lateinit var animatorSet: AnimatorSet
+    lateinit var animatorSet: AnimatorSet
 
-	fun setAnimation(animatorSet: AnimatorSet) {
-		this.animatorSet = animatorSet
-	}
+    fun setAnimation(animatorSet: AnimatorSet) {
+        this.animatorSet = animatorSet
+    }
 
-	fun setDuration(duration: Long) {
-		this.du = duration
-	}
+    fun setDuration(duration: Long) {
+        this.du = duration
+    }
 
-	fun isLoop(isLoop: Boolean) {
-		this.isLoop = isLoop
-	}
+    fun isLoop(isLoop: Boolean) {
+        this.isLoop = isLoop
+    }
 
-	fun setDelayLoop(delayLoop: Long) {
-		this.delayLoop = delayLoop
-	}
+    fun setDelayLoop(delayLoop: Long) {
+        this.delayLoop = delayLoop
+    }
 
-	fun start() {
-		animatorSet.duration = du
-		animatorSet.interpolator = AccelerateInterpolator()
-		animatorSet.addListener(object : AnimatorListenerAdapter() {
-			override fun onAnimationEnd(animation: Animator) {
-				super.onAnimationEnd(animation)
-				if (isLoop && !isPause) {
-					animatorSet.startDelay = delayLoop
-					animatorSet.start()
-				}
-			}
-		})
-		animatorSet.start()
-	}
+    fun start(animationEnd: () -> Unit = {}) {
+        animatorSet.removeAllListeners()
+        animatorSet.duration = du
+        animatorSet.interpolator = AccelerateInterpolator()
+        animatorSet.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                super.onAnimationEnd(animation)
+                if (isLoop && !isPause) {
+                    animatorSet.startDelay = delayLoop
+                    animatorSet.start()
+                }
+                animationEnd()
+            }
+        })
+        animatorSet.start()
+    }
 
-	fun pause() {
-		isPause = true
-		animatorSet.pause()
-	}
+    fun pause() {
+        isPause = true
+        animatorSet.pause()
+    }
 
-	fun resume() {
-		isPause = false
-		animatorSet.resume()
-	}
+    fun resume() {
+        isPause = false
+        animatorSet.resume()
+    }
 
-	fun cancel() {
-		isPause = true
-		animatorSet.cancel()
-	}
+    fun cancel() {
+        isPause = true
+        animatorSet.cancel()
+    }
 }

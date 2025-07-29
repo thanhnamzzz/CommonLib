4082d827fcb50979ccb660a5948a0697553eb7f1
Nguyễn Thành Nam <106824405+thanhnamzzz@users.noreply.github.com>
2025-07-29 16:19:20 +0700

    fix: build.gradle.kts

--- common_libs/build.gradle.kts
+++ common_libs/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
 	alias(libs.plugins.android.library)
 	alias(libs.plugins.kotlin.android)
-	id("maven-publish")
+	`maven-publish`
 }
 
 android {
@@ -30,11 +30,14 @@ android {
 		sourceCompatibility = JavaVersion.VERSION_17
 		targetCompatibility = JavaVersion.VERSION_17
 	}
-	kotlin {
-		compilerOptions {
-			jvmTarget.set(JvmTarget.JVM_17)
-		}
+	tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+		compilerOptions.jvmTarget.set(JvmTarget.JVM_17)
 	}
+//	kotlin {
+//		compilerOptions {
+//			jvmTarget.set(JvmTarget.JVM_17)
+//		}
+//	}
 
 	buildFeatures { viewBinding = true }
 }
